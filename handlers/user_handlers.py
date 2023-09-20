8045a4f7ee0072f2934075ed1b22d08549827ead
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-09-20 21:50:24 +0300

    Add stickers to  dict

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -4,8 +4,9 @@ import time
 from aiogram import F, Router
 from aiogram.filters import Command, CommandStart
 from aiogram.types import CallbackQuery, Message
-from keyboards.keyboards import create_inline_joke_kb
-from lexicon.lexicon import LEXICON_MESSAGES_RU, LEXICON_JOKES, LEXICON_STICKERS
+from keyboards.keyboards import create_inline_joke_kb, create_rating_kb
+from lexicon.lexicon import (LEXICON_MESSAGES_RU, LEXICON_JOKES,
+                             LEXICON_JOKES_STICKERS, LEXICON_STICKERS)
 from services.services import get_random_value
 
 router = Router()
@@ -36,17 +37,22 @@ async def process_joke_cmd(message: Message):
                          reply_markup=create_inline_joke_kb())
 
 
+# Handler for 'rate" cmd
+@router.message(Command(commands=['rate']))
+async def process_rate_cmd(message: Message):
+    await message.answer(text='Rate please',
+                         reply_markup=create_rating_kb(width=5))
+
+
 # Handler for callback data 'yes_joke'
 @router.callback_query(F.data == 'yes_joke')
 async def process_yes_joke_press(callback: CallbackQuery):
-    dictionary = LEXICON_JOKES
-
     time.sleep(1)
     await callback.message.answer(text=get_random_value(LEXICON_JOKES))
-    time.sleep(3)
+    time.sleep(2)
     await callback.bot.send_sticker(chat_id=callback.message.chat.id,
-                                    sticker=LEXICON_STICKERS['lol'])
-    time.sleep(3)
+                                    sticker=get_random_value(LEXICON_JOKES_STICKERS))
+    time.sleep(2)
     await callback.message.answer(text='Ещё?',
                                   reply_markup=create_inline_joke_kb())
 
@@ -60,12 +66,12 @@ async def process_no_joke_press(callback: CallbackQuery):
 
     time.sleep(1)
     await callback.message.answer(text=LEXICON_MESSAGES_RU['yes_to_no'])
-    time.sleep(2)
+    time.sleep(1)
     await callback.message.answer(text=get_random_value(dictionary))
-    time.sleep(3)
+    time.sleep(2)
     await callback.bot.send_sticker(chat_id=callback.message.chat.id,
-                                    sticker=LEXICON_STICKERS['lol'])
-    time.sleep(3)
+                                    sticker=get_random_value(LEXICON_JOKES_STICKERS))
+    time.sleep(2)
     await callback.message.answer(text='Ещё пошутить?',
                                   reply_markup=create_inline_joke_kb())
 
--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -12,3 +12,14 @@ def create_inline_joke_kb(width: int = 2) -> InlineKeyboardMarkup:
     kb_builder.row(button_1, button_2, width=width)
 
     return kb_builder.as_markup()
+
+
+def create_rating_kb(width: int = 2) -> InlineKeyboardMarkup:
+    kb_builder = InlineKeyboardBuilder()
+
+    buttons = [InlineKeyboardButton(text=str(value), callback_data=str(value))
+               for value in range(1, 6)]
+
+    kb_builder.row(*buttons, width=width)
+
+    return kb_builder.as_markup()
