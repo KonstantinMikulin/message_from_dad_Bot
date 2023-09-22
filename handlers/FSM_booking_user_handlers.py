cb080b67fa9e08f8b61042f3e0e7a721f023e579
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-09-22 19:48:17 +0300

    FSM booking few more rows

--- handlers/FSM_booking_user_handlers.py
+++ handlers/FSM_booking_user_handlers.py
@@ -28,4 +28,62 @@ async def process_picked_data(message: Message, state: FSMContext):
 # Handler if date was incorrect
 @router.message(StateFilter(FSMBooking.pick_date))
 async def warning_not_date(message: Message):
-    await message.answer(text=LEXICON_BOOKING_RU[' '])
+    await message.answer(text=LEXICON_BOOKING_RU['not_date'])
+
+
+# Handler if time was correct and switch to fill_name state
+@router.message(StateFilter(FSMBooking.pick_time), F.data.split(':').isdigit())
+async def process_sent_time(message: Message, state: FSMContext):
+    await state.update_data(time=message.text)
+    await message.answer(text=LEXICON_BOOKING_RU['fill_name'])
+    await state.set_state(FSMBooking.fill_name)
+
+
+# Handler if time was incorrect
+@router.message(StateFilter(FSMBooking.pick_time))
+async def warning_not_time(message: Message):
+    await message.answer(text=LEXICON_BOOKING_RU['not_time'])
+
+
+# Handler if name was correct and switch to fill_phone_number
+@router.message(StateFilter(FSMBooking.fill_name),
+                lambda x: x.text.split().isalpha and 3 < x.text.split() < 55)
+async def process_sent_name(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await message.answer(text=LEXICON_BOOKING_RU['fill_phone_number'])
+    await state.set_state(FSMBooking.fill_phone_number)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
